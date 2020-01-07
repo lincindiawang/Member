30803c14199fb633ec315ed57ee4539b78a95fe5
lincindiawang <lindawang0207@gmail.com>
2020-01-07 14:15:28 +0800

    1st

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -2,13 +2,16 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    String abc;
+//    String abc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSharedPreferences("Jack",MODE_PRIVATE);
+        startActivity(new Intent(this,NicknameActivity.class));
     }
 }
